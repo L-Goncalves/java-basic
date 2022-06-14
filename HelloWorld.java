3ef4f506d3a5a030f5a0edb5ce0947b0f11a3a85
ldgoncalvs@gmail.com <ldgoncalvs@gmail.com>
2022-06-14 12:28:31 -0300

    add: Hello World in Java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+    public static void main(String [] args){
+        System.out.println("Hello");
+        System.out.print("World");
+        System.out.println("!"); 
+    }
+}
+
+
+// variable is defined like: type name = value
+// example:                  int someNumber = 0
