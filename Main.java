2d93e23f06e12e484a7e3a933c564916c5230fe8
Lucas Gonçalves <ldgoncalvs@gmail.com>
2022-06-15 00:21:29 -0300

    Revert "just playing with java to see what happens"
    This reverts commit 3758573f32ee7f4911a4b1bf904c3a83cabfb534.

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-  
+       
+        // reads valeu from input
+        Scanner input = new Scanner(System.in);
         Pen p = new Pen();
         // input
-        
-
-        p.setClicked("Y");
+        p.setClicked(false);
         p.setColor("black");
         p.setType("normal");
 
